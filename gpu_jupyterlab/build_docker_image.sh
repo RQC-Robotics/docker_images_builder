42d743816da2476cad6a186bac21d38fb26cde65
korbash <korbash.sen@gmail.com>
2022-05-28 05:39:19 +0000

    move to tensorflow base image

--- gpu_jupyterlab/build_docker_image.sh
+++ gpu_jupyterlab/build_docker_image.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-gpu=false
+gpu=true
 rebuild=true
 # port should be unique for every user
 uniq_port='17957'
-container_name="tf_sensor_cpu"
-project_name='tf_sensor_cpu'
-
+container_name="tf_sensor_gpu3"
+project_name='tf_sensor_gpu2'
+key_dir='projects'
 # in home directory shoud exist
 # .ssh
 # .aws
 # .gitconfig
 
-image_name='korbash/minimal_notebook'
+image_name='korbash/minimal_notebook_gpu:0.1'
 project_dir="projects"
 
 dir="${project_dir}/${project_name}"
@@ -23,31 +23,37 @@ if ! [ -d ~/${dir} ]; then
     cd ${old}
 fi
 if ! [ -d ~/${dir}/.ssh ]; then
-    cp -r ~/.ssh ~/${dir}/.ssh
+    cp -r ~/${key_dir}/.ssh ~/${dir}/.ssh
 fi
 if ! [ -d ~/${dir}/.aws ]; then
-    cp -r ~/.aws ~/${dir}/.aws
+    cp -r ~/${key_dir}/.aws ~/${dir}/.aws
 fi
 if ! [ -d ~/${dir}/.gitconfig ]; then
-    cp ~/.gitconfig ~/${dir}/.gitconfig
+    cp ~/${key_dir}/.gitconfig ~/${dir}/.gitconfig
 fi
 
 if ${gpu}; then
     if ${rebuild}; then
-        docker build -t ${image_name} --build-arg ROOT_CONTAINER=ubuntu:focal .
+        docker build -t ${image_name} --build-arg ROOT_CONTAINER=tensorflow/tensorflow:latest-gpu .
     else
         image_name='korbash/minimal_notebook_gpu'
         docker pull ${image_name}
     fi
+    docker run  --name "${container_name}" --user root -e GRANT_SUDO=yes \
+     --gpus all \
+     -p "${uniq_port}:8888" -p 17956:6006 \
+     -v ~/${dir}:/home/jovyan \
+     "${image_name}"
+
 else
     if ${rebuild}; then
-        docker build -t ${image_name} --build-arg ROOT_CONTAINER=nvidia/cuda:11.2.2-runtime-ubuntu20.04 .
+        docker build -t ${image_name} --build-arg ROOT_CONTAINER=ubuntu:focal .
     else
         image_name='korbash/minimal_notebook'
         docker pull ${image_name}
     fi
+    docker run  --name "${container_name}" --user root -e GRANT_SUDO=yes \
+     -p "${uniq_port}:8888" \
+     -v ~/${dir}:/home/jovyan \
+     "${image_name}"
 fi
-docker run  --name "${container_name}" --user root -e GRANT_SUDO=yes \
- -p "${uniq_port}:8888" \
- -v ~/${dir}:/home/jovyan \
- "${image_name}"
