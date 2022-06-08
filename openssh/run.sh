bca89127bbcf1e4ac7ebd61739951b1c11e16414
korbash <korbash.sen@gmail.com>
2022-06-08 15:49:26 +0000

    add acses to secret keys

--- openssh/run.sh
+++ openssh/run.sh
@@ -1,3 +1,7 @@
 #! bin/bash
 
-docker run -p 23:22 -d --gpus=all --ipc=host --name isaac korbash/isaacgym:0.0
+docker run -p 23:22 -d --gpus=all --ipc=host --name isaac \
+ -v ~/.ssh:/home/gymuser/.ssh \
+ -v ~/.aws:/home/gymuser/.aws \
+ -v ~/.gitconfig:/home/gymuser/.gitconfig \
+ korbash/isaacgym:0.0
